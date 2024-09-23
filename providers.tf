18e50c454335403b72a098d65404b1265fc14118
mrolmedo <96732346+mrolmedo@users.noreply.github.com>
2024-09-23 12:41:23 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,12 +4,21 @@ terraform {
 
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "3.1.1"
+      version = "5.0.0"
     }
   } # End of required_providers
 }   # End of terraform
 
+provider "vsphere" {
+  vsphere_server       = var.vsphere_env.server
+  user                 = var.vsphere_env.user
+  password             = var.vsphere_env.pass
+  allow_unverified_ssl = true
+}
+
+
 provider "rancher2" {
-  api_url   = file("${path.cwd}/files/.rancher-api-url")
-  token_key = file("${path.cwd}/files/.rancher-bearer-token")
+  api_url   = "rancherurl"
+  insecure  = true
+  token_key = "token"
 }
